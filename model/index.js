f25079d5bd9411206f4a3b03abd7a3f8172e1bfe
naseeb-shah <102802675+naseeb-shah@users.noreply.github.com>
2022-09-26 23:55:58 +0530

    Update index.js
    ssss

--- model/index.js
+++ model/index.js
@@ -5,7 +5,7 @@ async function condatabase(){
     return new Promise((resolve, reject)=>{
 
         const url='mongodb+srv://deenshah:Deenshah@cluster0.f3dzadd.mongodb.net/labs?retryWrites=true&w=majority'
->>>>>>> a07f927ca1b25f22fb834c4c086be95b1f5dbe19
+
     // const url='mongodb://localhost:27017/deen'
       mongoose.connect(url,(err)=>{
 
